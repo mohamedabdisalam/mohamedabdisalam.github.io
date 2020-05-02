b940dc0d6b90e91d6a475d82b0a2623a800cc084
mohamedabdisalam <mohamedabdisalam@users.noreply.github.com>
2020-05-02 15:50:53 +0100

    added some responsiveness added auto slide feature to image carousel refactored redundant code

--- app.js
+++ app.js
@@ -5,75 +5,99 @@ function myDarkMode() {
 }
 
 //Carousel
-const carouselSlide = document.querySelector('.carousel-slide');
-const carouselImages = document.querySelectorAll('.carousel-slide img');
+// const carouselSlide = document.querySelector('.carousel-slide');
+// const carouselImages = document.querySelectorAll('.carousel-slide img');
 
-//Buttons
+// //Auto slide
+let myIndex = 0;
+    carousel();
 
-const prevBtn = document.querySelector('#prevBtn');
-const nextBtn = document.querySelector('#nextBtn');
+function carousel() {
+    const mySlides = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < mySlides.length; i++) {
+        mySlides[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > mySlides.length) {myIndex = 1}    
+    mySlides[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 3000);
+  }
 
-//Counter
+// //Buttons
 
-let counter = 1;
+// const prevBtn = document.querySelector('#prevBtn');
+// const nextBtn = document.querySelector('#nextBtn');
 
-let size = carouselImages[0].offsetWidth;
+// //Counter
 
- carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';      
+// let counter = 1;
 
- //Play/Pause feature
-let playing = true;
-let pauseButton = document.getElementById('pause');
+// let size = carouselImages[0].offsetWidth;
 
-function pauseSlideshow(){
-	pauseButton.innerHTML = 'Play';
-	playing = false;
-	clearInterval(slideInterval);
-}
-
-function playSlideshow(){
-	pauseButton.innerHTML = 'Pause';
-	playing = true;
-	slideInterval = setInterval(nextSlide,2000);
-}
-
-pauseButton.onclick = function(){
-	if(playing){ pauseSlideshow(); }
-	else{ playSlideshow(); }
-};
-
-//Loop feature
-let toggleCarousel = true;
-
-
+//  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'; 
  
- //Button listeners
+//  //Button listeners 
+// nextBtn.addEventListener('click', () => {
+//     if (counter >= carouselImages.length - 1) return;
+//     carouselSlide.style.transition = "transform 0.4s ease-in-out";
+//     counter++;
+//     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+// });
 
-nextBtn.addEventListener('click', () => {
-    if (counter >= carouselImages.length - 1) return;
-    carouselSlide.style.transition = "transform 0.4s ease-in-out";
-    counter++;
-    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-});
-
-prevBtn.addEventListener('click', () => {
-    if (counter <= 0) return; 
-    carouselSlide.style.transition = "transform 0.4s ease-in-out";
-    counter--;
-    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-});
-
+// prevBtn.addEventListener('click', () => {
+//     if (counter <= 0) return; 
+//     carouselSlide.style.transition = "transform 0.4s ease-in-out";
+//     counter--;
+//     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+// });
 
 
-carouselSlide.addEventListener('transitionend', () => {
-    if (carouselImages[counter].id === 'lastClone') {
-        carouselSlide.style.transition = "none"; 
-        counter = carouselImages.length - 2;
-        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-    }
-    if (carouselImages[counter].id === 'firstClone') {
-        carouselSlide.style.transition = "none"; 
-        counter = carouselImages.length - counter;
-        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-    }
-});
+
+// carouselSlide.addEventListener('transitionend', () => {
+//     if (carouselImages[counter].id === 'lastClone') {
+//         carouselSlide.style.transition = "none"; 
+//         counter = carouselImages.length - 2;
+//         carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+//     }
+//     if (carouselImages[counter].id === 'firstClone') {
+//         carouselSlide.style.transition = "none"; 
+//         counter = carouselImages.length - counter;
+//         carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+//     }
+// });
+
+// // function changeImage(dir) {
+// //     var img = document.getElementById("imgClickAndChange");
+// //     img.src = imgs[imgs.indexOf(img.src) + (dir || 1)] || imgs[dir ? imgs.length - 1 : 0];
+// // }
+
+// // document.onkeydown = function(e) {
+// //     e = e || window.event;
+// //     if (e.keyCode == '37') {
+// //         changeImage(-1) //left <- show Prev image
+// //     } else if (e.keyCode == '39') {
+// //         // right -> show next image
+// //         changeImage()
+// //     }
+// // }
+
+//  //Play/Pause feature
+// // let playing = true;
+// // let pauseButton = document.getElementById('pause');
+
+// // function pauseSlideshow(){
+// // 	pauseButton.innerHTML = 'Play';
+// // 	playing = false;
+// // 	clearInterval(slideInterval);
+// // }
+
+// // function playSlideshow(){
+// // 	pauseButton.innerHTML = 'Pause';
+// // 	playing = true;
+// // 	slideInterval = setInterval(nextSlide,2000);
+// // }
+
+// // pauseButton.onclick = function(){
+// // 	if(playing){ pauseSlideshow(); }
+// // 	else{ playSlideshow(); }
+// // };
